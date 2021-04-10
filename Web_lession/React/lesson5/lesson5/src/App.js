5c1d67290536fc65bf8aa1bff8279d8d4a40e1df
leafsu <leafgeek@foxmail.com>
2021-04-10 09:30:13 +0800

    delete web_lession

--- Web_lession/React/lesson5/lesson5/src/App.js
+++
@@ -1,5 +0,0 @@
-import Routes from "./routes";
-
-export default function App(props) {
-  return <Routes />;
-}
--- Web_lession/React/lesson6-umi/src/services/product.js
+++
@@ -1,8 +0,0 @@
-import request from "../utils/request";
-
-export async function getProductData(params) {
-  return request("/api/getProductData", {
-    data: params,
-    method: "post"
-  });
-}
