96601c40cd2f9695f97fc7237c50edd9b6fac893
leafsu <leafgeek@foxmail.com>
2021-04-19 07:38:33 +0800

    feat:hook-example

---
+++ Projects/DebugReact/src/pages/HookExamplePage.js
@@ -0,0 +1,59 @@
+import * as React from "react";
+import { useState, useCallback, PureComponent } from "react";
+export default function UseCallbackPage(props) {
+  const [count, setCount] = useState(0);
+  const addClick = useCallback(() => {
+    let sum = 0;
+    for (let i = 0; i < count; i++) {
+      sum += i;
+    }
+    return sum;
+  }, [count]);
+  const [value, setValue] = useState("");
+  return (
+    <div>
+      <h3>UseCallbackPage</h3>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>add</button>
+      <input value={value} onChange={(event) => setValue(event.target.value)} />
+      <Child addClick={addClick} />
+    </div>
+  );
+}
+class Child extends PureComponent {
+  render() {
+    console.log("child render");
+    const { addClick } = this.props;
+    return (
+      <div>
+        <h3>Child</h3>
+        <button onClick={() => console.log(addClick())}>add</button>
+      </div>
+    );
+  }
+}
+
+
+// import * as React from "react";
+// import { useState, useMemo } from "react";
+// export default function UseMemoPage(props) {
+//   const [count, setCount] = useState(0);
+//   const [value, setValue] = useState("");
+//   const expensive = useMemo(() => {
+//     console.log("compute");
+//     let sum = 0;
+//     for (let i = 0; i < count; i++) {
+//       sum += i;
+//     }
+//     return sum;
+//   }, [count]);
+//   return (
+//     <div>
+//       <h3>UseMemoPage</h3>
+//       <p>expensive:{expensive()}</p>
+//       <p>{count}</p>
+//       <button onClick={() => setCount(count + 1)}>add</button>
+//       <input value={value} onChange={(event) => setValue(event.target.value)} />
+//     </div>
+//   );
+// }
