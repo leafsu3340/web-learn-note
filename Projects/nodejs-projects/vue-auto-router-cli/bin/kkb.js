1454d634e73787b2a77c07c981884b9b19940a17
leafsu <leafgeek@foxmail.com>
2021-04-22 22:57:40 +0800

    feat: node基础练习

--- Projects/nodejs-projects/vue-auto-router-cli/bin/kkb.js
+++ Projects/nodejs-projects/vue-auto-router-cli/bin/kkb.js
@@ -1,18 +1,17 @@
 #!/usr/bin/env node
-const program = require('commander')
-program.version(require('../package').version)
+console.log("666");
+const program = require("commander");
+program.version(require("../package.json").version);
+
 program
-    .command('init <name>')
-    .description('init project')
-    .action(
-        require('../lib/init')
-    )
+  .command("init <name>")
+  .description("init project")
+  .action(require("../lib/init"));
+
 program
-    .command('refresh')
-    .description('refresh routers...')
-    .action(require('../lib/refresh'))
-program
-    .command('serve')
-    .description('serve')
-    .action(require('../lib/serve'))
-program.parse(process.argv)
+  .command("refresh")
+  .description("refresh routers...")
+  .action(require("../lib/refresh"));
+
+program.command("serve").description("serve").action(require("../lib/serve"));
+program.parse(process.argv);
